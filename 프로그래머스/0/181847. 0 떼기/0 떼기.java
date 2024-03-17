4256fd4c85441c7ddbb88fcdc23e1aead3eecb3d
Jimin Lee <rjsb0648@gmail.com>
2024-03-17 13:05:05 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        int number = Integer.parseInt(n_str);
+        String str = Integer.toString(number);
+        
+        return str;
+    }
+}
