a7424dc3c478c908b93ed5c786869272019bb3fe
Jimin Lee <rjsb0648@gmail.com>
2024-03-27 14:36:19 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,16 @@
+import java.lang.Math;
+
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = a + b + c;
+
+        if(a == b || b == c || a == c) {
+            answer = answer * (a*a + b*b + c*c);
+        } 
+        if(a == b && a == c) {            
+            answer = answer * (a*a*a + b*b*b + c*c*c);
+        }
+ 
+        return answer;
+    }
+}
