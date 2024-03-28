7604dfc68f24d8b39764e69906bc95f6817da3b8
Jimin Lee <rjsb0648@gmail.com>
2024-03-29 01:05:49 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.02 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -1,15 +1,9 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        List<Integer> list = new ArrayList<>();
-        for(int i = n-1; i < num_list.length; i++) {
-            list.add(num_list[i]); 
-        }
+        int[] answer = new int[num_list.length - (n-1)];
         
-        int[] answer = new int[list.size()];
-        for(int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i).intValue();
+        for(int i = n-1; i < num_list.length; i++) {
+            answer[i - (n-1)] = num_list[i];
         }
         
         return answer;
