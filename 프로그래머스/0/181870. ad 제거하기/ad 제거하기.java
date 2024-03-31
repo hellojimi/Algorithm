39929f55d9eb52ec8854563f4e06eeda8a8d9108
Jimin Lee <rjsb0648@gmail.com>
2024-03-31 14:23:50 +0900

    [level 0] Title: ad 제거하기, Time: 1.51 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        ArrayList<String> answer = new ArrayList<>();
+        
+        for(int i = 0; i < strArr.length; i++) {
+            if(strArr[i].contains("ad")) {
+                strArr[i] = "";
+            } else {
+                answer.add(strArr[i]);
+            }
+        }
+        return answer.toArray(new String[0]);
+    }
+}
