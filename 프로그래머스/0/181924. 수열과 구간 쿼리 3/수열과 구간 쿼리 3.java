f7b168a101d7430333d0dc465cb05be92d9885f8
Jimin Lee <rjsb0648@gmail.com>
2024-04-02 18:05:58 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.06 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -2,8 +2,7 @@ class Solution {
     public int[] solution(int[] arr, int[][] queries) {
 
         for(int i = 0; i < queries.length; i++) {
-            int temp = 0;
-            temp = arr[queries[i][0]];
+            int temp = arr[queries[i][0]];
             arr[queries[i][0]] = arr[queries[i][1]];
             arr[queries[i][1]] = temp;
         }
