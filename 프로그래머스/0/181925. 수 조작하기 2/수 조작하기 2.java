527c83593d9b06364a3b4e6ebae0aabbbc02ce9b
Jimin Lee <rjsb0648@gmail.com>
2024-04-02 17:38:29 +0900

    [level 0] Title: 수 조작하기 2, Time: 642.47 ms, Memory: 389 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -3,14 +3,19 @@ class Solution {
         String answer = "";
         
         for(int i = 0; i < numLog.length - 1; i++) {
-            if(numLog[i] + 1 == numLog[i+1]) {
-                answer += "w";
-            } else if(numLog[i] - 1 == numLog[i+1]) {
-                answer += "s";
-            } else if(numLog[i] + 10 == numLog[i+1]) {
-                answer += "d";
-            } else {
-                answer += "a";
+            switch(numLog[i+1] - numLog[i]) {
+                case 1:
+                    answer += "w";
+                    break;
+                case -1:
+                    answer += "s";
+                    break;
+                case 10:
+                    answer += "d";
+                    break;
+                case -10:
+                    answer += "a";
+                    break;
             }
         }
         return answer;
