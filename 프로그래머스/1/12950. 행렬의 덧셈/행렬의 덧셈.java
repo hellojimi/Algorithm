4b53f7d64eafd4326c99fc7ccb76f032c9d1ab6c
Jimin Lee <rjsb0648@gmail.com>
2024-05-31 15:21:35 +0900

    [level 1] Title: 행렬의 덧셈, Time: 21.39 ms, Memory: 134 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][];
+        
+        for(int i = 0; i < arr1.length; i++) {
+            List<Integer> list = new ArrayList();
+            
+            for(int j = 0; j < arr1[i].length; j++) {
+                list.add(arr1[i][j] + arr2[i][j]);
+            }
+            
+            answer[i] = list.stream().mapToInt(x -> x).toArray();
+        }
+        
+        return answer;
+    }
+}
